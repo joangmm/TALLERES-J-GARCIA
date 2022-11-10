ea7580cfaea886aa8fba5e00c77f1df556d977c1
joangmm <gmjoan77@gmail.com>
2022-11-10 11:41:15 +0100

    spanish now

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ import {Head, Html, Main, NextScript} from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es">
       <Head>
         <meta charSet="utf-8" />
         {/* google translate breaks react:
