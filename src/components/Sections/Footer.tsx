2417c18e01a3f9996c7e6fd2effc5381f85c7363
joangmm <gmjoan77@gmail.com>
2022-11-10 10:47:11 +0100

    hasta aquí bn

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -2,7 +2,6 @@ import {ChevronUpIcon} from '@heroicons/react/outline';
 import {FC, memo} from 'react';
 
 import {SectionId} from '../../data/data';
-import Socials from '../Socials';
 
 const Footer: FC = memo(() => (
   <div className="relative bg-neutral-900 px-4 pb-6 pt-12 sm:px-8 sm:pt-14 sm:pb-8">
@@ -15,9 +14,9 @@ const Footer: FC = memo(() => (
     </div>
     <div className="flex flex-col items-center gap-y-6">
       <div className="flex gap-x-4 text-neutral-500">
-        <Socials />
+        {/* <Socials /> */}
       </div>
-      <span className="text-sm text-neutral-700">© Copyright 2022 Tim Baker</span>
+      <span className="text-sm text-neutral-700">©2022 Joan Garcia Marty. Todos los derechos reservados.</span>
     </div>
   </div>
 ));
